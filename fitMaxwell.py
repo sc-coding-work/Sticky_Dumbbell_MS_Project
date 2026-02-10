2dfc9bc4b76f8e64d1691aa5203d898de7f71d29
SC <cronksam783@gmail.com>
2026-02-10 14:22:20 +0000

    Created ExptBohlin_312 fits and graph

--- fitMaxwell.py
+++ fitMaxwell.py
@@ -18,7 +18,7 @@ from scipy.optimize import least_squares
 import os
 
 # ------------------ User settings ------------------
-filename = "ExptBohlin_311.bbx"     # file with omega, G', G''
+filename = "ExptBohlin_312.bbx"     # file with omega, G', G''
 n_modes = 2               # number of Maxwell modes to fit
 omega_min = 0.0          # e.g. 0.1  — set to None to disable lower bound
 omega_max = None          # e.g. 1000 — set to None to disable upper bound
